a2fbd7975277666e62133fecc8b10d4d3989bddf
David <brahian.quintero8989@gmail.com>
2026-06-05 23:38:20 +0200

    fix error 404 vercel

--- backend/src/modules/auth/auth.controller.js
+++ backend/src/modules/auth/auth.controller.js
@@ -37,8 +37,8 @@ const register = async (req, res, next) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: false,
-      sameSite: 'lax',
+      secure: true,
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
